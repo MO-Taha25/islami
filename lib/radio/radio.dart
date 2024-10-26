55dfe762de0425c7db3777b443ebd0eb9e0517d4
Mohamed Taha <mot60080@gmail.com>
2024-10-26 18:24:00 +0300

    Finish radio

--- lib/radio/radio.dart
+++ lib/radio/radio.dart
@@ -5,8 +5,34 @@ class RadioScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Radio'),
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/radio_image.png'),
+          ],
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'إذاعة القرآن الكريم',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 100,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('assets/images/Group 5.png')],
+        ),
+      ]),
     );
   }
 }
